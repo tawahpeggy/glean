5d2e0a3d5265a35ec6df9dc4db595821a702e88a
CircleCI docs-deploy job <jrediger@mozilla.com>
2020-11-16 14:40:28 +0000

    [skip ci] Updates

---
+++ docs/implementors/glean_core/traits/string/trait.String.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["glean"] = [{"text":"impl String for StringMetric","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
