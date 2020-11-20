d125d4af6fb1dd871c821b105368cc869bdfe860
CircleCI docs-deploy job <jrediger@mozilla.com>
2020-11-20 16:07:35 +0000

    [skip ci] Updates

---
+++ docs/implementors/glean_core/traits/uuid/trait.Uuid.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["glean"] = [{"text":"impl Uuid for UuidMetric","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
