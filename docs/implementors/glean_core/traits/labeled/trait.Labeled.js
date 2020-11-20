7adcbbd564125ce0248a3a01e88e05e10cd01b26
CircleCI docs-deploy job <jrediger@mozilla.com>
2020-11-20 17:32:14 +0000

    [skip ci] Updates

---
+++ docs/implementors/glean_core/traits/labeled/trait.Labeled.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["glean"] = [{"text":"impl&lt;T&gt; Labeled&lt;T&gt; for LabeledMetric&lt;T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: AllowLabeled + Clone,&nbsp;</span>","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
