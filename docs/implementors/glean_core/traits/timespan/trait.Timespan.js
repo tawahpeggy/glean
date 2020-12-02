3209a2aec50de4ba5ca1686803765adf57ab8a78
CircleCI docs-deploy job <jrediger@mozilla.com>
2020-12-02 11:42:14 +0000

    [skip ci] Updates

---
+++ docs/implementors/glean_core/traits/timespan/trait.Timespan.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["glean"] = [{"text":"impl Timespan for TimespanMetric","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
