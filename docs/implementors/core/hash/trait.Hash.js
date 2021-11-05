d648032862b2d35f7aa5cfc09bfbcc158ae78261
CircleCI docs-deploy job <jrediger@mozilla.com>
2021-11-05 09:39:07 +0000

    [skip ci] Updates

--- docs/implementors/core/hash/trait.Hash.js
+++ docs/implementors/core/hash/trait.Hash.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["glean_core"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.56.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"glean_core/traits/enum.NoExtraKeys.html\" title=\"enum glean_core::traits::NoExtraKeys\">NoExtraKeys</a>","synthetic":false,"types":["glean_core::traits::event::NoExtraKeys"]}];
+implementors["glean_core"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.56.1/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"glean_core/traits/enum.NoExtraKeys.html\" title=\"enum glean_core::traits::NoExtraKeys\">NoExtraKeys</a>","synthetic":false,"types":["glean_core::traits::event::NoExtraKeys"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
